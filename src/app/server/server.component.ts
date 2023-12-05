f31b225b3da9f8ff6d5fe3a3ed44304b1582bbaf
gitbernix <gittaberni@gmail.com>
2023-12-05 11:49:16 +0100

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  number1: number = 10;
+  number2: number = 15;
+
+  addNumbers() {
+    return this.number1 + this.number2;
+  }
+}
